daa38e685fa5f5d0f8c19100700e28f662b30af5
noise23 <noise23@users.noreply.github.com>
2023-05-16 06:08:39 +0300

    add GetRandBytes() as wrapper for RAND_bytes()
    - add a small wrapper in util around RAND_bytes() and replace with
      GetRandBytes() in the code to log errors from calling RAND_bytes()
    - remove OpenSSL header rand.h where no longer needed
    
    https://github.com/bitcoin/bitcoin/commit/001a53d7427dcbcceef3c6754d9cca19df6dafa1

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -830,7 +830,7 @@ void ThreadRPCServer2(void* parg)
     if (mapArgs["-rpcpassword"] == "")
     {
         unsigned char rand_pwd[32];
-        RAND_bytes(rand_pwd, 32);
+        GetRandBytes(rand_pwd, 32);
         string strWhatAmI = "To use Truckcoind";
         if (mapArgs.count("-server"))
             strWhatAmI = strprintf(_("To use the %s option"), "\"-server\"");
